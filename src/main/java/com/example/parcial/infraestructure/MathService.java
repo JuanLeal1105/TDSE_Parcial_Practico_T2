080b2627d0a97256fcd3baa6c75f4fa97b03c751
unknown <juan.lcruz@labinfo.is.escuelaing.edu.co>
2026-03-24 09:52:52 -0500

    fix chain for lucas

--- src/main/java/com/example/parcial/infraestructure/MathService.java
+++ src/main/java/com/example/parcial/infraestructure/MathService.java
@@ -2,8 +2,17 @@ package com.example.parcial.infraestructure;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MathService {
+    public List<Integer> getLucas (int value){
+        List<Integer> chain = new ArrayList<>();
+        chain.add(calculateLucas(value));
+        return chain;
+    }
+
     public int calculateLucas (int value){
         if (value == 0){
             return 2;
